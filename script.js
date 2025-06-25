237e393b0ad52fba78f6cff164b3b131e23da14d
Manish Thakur <manisthakur2004@gmail.com>
2025-06-25 11:21:34 +0545

    responsive

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const femaleContainer = document.querySelector(".container");
 const maleContainer = document.querySelector(".mens-container");
 
+     function showSidebar() {
+            const sidebar = document.querySelector(".sidebar");
+            sidebar.style.display = "block";
+        }
+
+        function hideSidebar() {
+            const sidebar = document.querySelector(".sidebar");
+            sidebar.style.display = "none";
+        }
